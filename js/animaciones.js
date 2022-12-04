9ef48343f52b379b7e3b476883d4d8458125b416
juan-toro <jftoro940@misena.edu.co>
2022-12-03 20:22:47 -0500

    eventos y promotores funcional

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,73 +1,65 @@
- let promotores=document.getElementsByClassName("promotor")
+//eventos
 
-for(let promotor of promotores){
-    promotor.addEventListener("click",mostrarPromotor)
-    function mostrarPromotor(){
-    
-        if(promotor.style.position != "absolute"){
-         promotor.style.position="absolute"
-         promotor.style.width="700px"
-         promotor.style.Maxheight="92vh"
-         promotor.style.Overflow="auto"
-         promotor.style.Zindex="4"
-         promotor.style.margin="0px auto"
-         document.querySelector(".promotor-info").style.height="auto"
-         document.querySelector(".promotor-footer").style.height="40px"
-     
-        }else{
-         promotor.style.position="relative"
-         promotor.style.width="45%"
-         promotor.style.height="auto"
-         promotor.style.Overflow="hidden"
-         promotor.style.Zindex="1"
-         promotor.style.margin="30px 0px"
-         document.querySelector(".promotor-info").style.height="120px"
-         document.querySelector(".promotor-footer").style.height="40px"
-        
+let events=document.querySelectorAll(".event")
+let btnEvent=document.querySelectorAll(".btn-event")
+let closeEvent=document.querySelectorAll(".close-event")
+
+
+for (let i=0; i<btnEvent.length; i++){
+    btnEvent[i].addEventListener("click",()=>{
+        events[i].setAttribute("id","true")
+        if(events[i].id=="true"){
+            closeEvent[i].style.display="block"
         }
-     }
+  
+    })
+    closeEvent[i].addEventListener("click",()=>{
+        events[i].removeAttribute("id")
+        closeEvent[i].style.display="none"
+    })
+    
+}
 
+//promotores
+let promotores=document.querySelectorAll(".promotor")
+let showPromotor=document.querySelectorAll(".show-promotor")
+let closePromotor=document.querySelectorAll(".close-promotor")
+
+for(let i=0; i<promotores.length; i++){
+    showPromotor[i].addEventListener("click",()=>{
+        promotores[i].setAttribute("id","promotorTrue")
+        if(promotores[i].id=="promotorTrue"){
+            showPromotor[i].style.display="none"
+            closePromotor[i].style.display="block"
+        }
+    })
+
+    closePromotor[i].addEventListener("click",()=>{
+        promotores[i].removeAttribute("id")
+        showPromotor[i].style.display="block"
+        closePromotor[i].style.display="none"
+
+    })
 }
 
 
- 
+//buscador Eventos
+
+let search=document.getElementById("search")
+search.addEventListener("keyup",buscarEvento)
+
+function buscarEvento(){
+    let containerEventos=document.getElementById("container-eventos")
+    let items=``
+
+    for(){
+        items+=``
+    }
 
 
-let btns=document.getElementsByClassName("btn-event")
-for(let btn_event of btns){
 
-    btn_event.addEventListener("click",reserva)
-    function reserva(){
-        if(btn_event.style.display!="none"){
-           btn_event.style.display="none"
-           let ns=document.getElementsByClassName("event")
-           for (let n of ns){
-            n.style.Maxheight="auto"
-           }
-           let tickets=document.getElementsByClassName("container-buy-ticket")
-           for(let t of tickets){
-            t.style.height="140px"
-            t.style.Minheight="140px"
-            t.style.padding="20px"
-           }
-          
-       }else{
-           btn_event.style.display="block"
-           let ns=document.getElementsByClassName("event")
-           for (let n of ns){
-            n.style.Maxheight="auto"
-            
-           }
-           let tickets=document.getElementsByClassName("container-buy-ticket")
-           for(let t of tickets){
-            t.style.height="0px"
-            t.style.padding="0px"
-           }
-           
-           
-       } 
-       
-   }
-
+    containerEventos.innerHTML=items
 }
+
+
 
